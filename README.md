9bc13cb0d25ec08ad6795415ba5d30ce55cffe21
Patrick Huang <huangpatrick16777216@gmail.com>
2023-03-27 19:55:03 -0700

    Sort writings.

--- README.md
+++ README.md
@@ -3,3 +3,12 @@
 Theories from an armchair philosopher.
 
 None of this is rigorous. These are simply thoughts that have came into my mind.
+
+### Education
+
+- [Teaching](./src/teaching.txt)
+
+### Physics
+
+- [Time](./src/time.txt)
+
---
+++ scripts/readme.py
@@ -0,0 +1,42 @@
+from pathlib import Path
+
+ROOT = Path(__file__).absolute().parent.parent
+
+WRITINGS = {
+    "teaching": ["education"],
+    "time": ["physics"],
+}
+
+TEMPLATE = """
+# Theories
+
+Theories from an armchair philosopher.
+
+None of this is rigorous. These are simply thoughts that have came into my mind.
+""".strip() + "\n\n"
+
+
+def main():
+    categories = set()
+    for c in WRITINGS.values():
+        categories.update(c)
+    categories = sorted(categories)
+
+    text = TEMPLATE
+    for c in categories:
+        writings = []
+        for w in WRITINGS.keys():
+            if c in WRITINGS[w]:
+                writings.append(w)
+        writings = sorted(writings)
+
+        text += f"### {c.capitalize()}\n\n"
+        for w in writings:
+            text += f"- [{w.capitalize()}](./src/{w}.txt)\n"
+        text += "\n"
+
+    (ROOT / "README.md").write_text(text)
+
+
+if __name__ == "__main__":
+    main()
